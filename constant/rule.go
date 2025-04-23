8604633f9fc9f1b564848e3a7b064b2a2450e45a
yelnoo <t50500411@163.com>
2025-04-23 20:07:47 +0800

    clash-api: Add rule-provider support

--- constant/rule.go
+++ constant/rule.go
@@ -43,3 +43,14 @@ const (
 	RuleActionRejectMethodDrop    = "drop"
 	RuleActionRejectMethodReply   = "reply"
 )
+
+func RuleSetFormatDisplayName(format string) string {
+	switch format {
+	case RuleSetFormatSource:
+		return "Source"
+	case RuleSetFormatBinary:
+		return "Binary"
+	default:
+		return "Unknown"
+	}
+}
